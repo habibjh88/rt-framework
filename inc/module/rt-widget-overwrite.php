7b1f3dc662801772b820c7118ce7e5bcc2d33cf1
Habib Rahaman <habibjh88@gmail.com>
2024-01-17 18:01:32 +0600

    Customizer css modify

--- inc/module/rt-widget-overwrite.php
+++ inc/module/rt-widget-overwrite.php
@@ -29,33 +29,42 @@ if ( ! class_exists( 'Rt_Widgets_Overwrite' ) ) {
 				$instance['widget_lg_cols'] = null;
 			}
 			?>
-            <p>
+            <div class="rt-widget-custom-cols">
                 <label for="<?php echo $t->get_field_id( 'widget_xl_cols' ); ?>"><?php echo esc_html__( 'Column:', 'newsfit' ) ?></label>
-            <div class="widget-cols" style="display:flex;gap:15px;margin-bottom:5px;">
-                <select id="<?php echo $t->get_field_id( 'widget_xl_cols' ); ?>" name="<?php echo $t->get_field_name( 'widget_xl_cols' ); ?>">
-                    <option value=""><?php echo esc_html__( '-- Extra Large Columns --', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_xl_cols'], 'col-xl-2' ); ?> value="col-xl-2"><?php echo esc_html__( 'col-xl-2', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_xl_cols'], 'col-xl-3' ); ?> value="col-xl-3"><?php echo esc_html__( 'col-xl-3', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_xl_cols'], 'col-xl-4' ); ?> value="col-xl-4"><?php echo esc_html__( 'col-xl-4', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_xl_cols'], 'col-xl-5' ); ?> value="col-xl-5"><?php echo esc_html__( 'col-xl-5', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_xl_cols'], 'col-xl-6' ); ?> value="col-xl-6"><?php echo esc_html__( 'col-xl-6', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_xl_cols'], 'col-xl-7' ); ?> value="col-xl-7"><?php echo esc_html__( 'col-xl-7', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_xl_cols'], 'col-xl-8' ); ?> value="col-xl-8"><?php echo esc_html__( 'col-xl-8', 'newsfit' ); ?></option>
-                </select>
+                <div class="widget-cols" style="display:flex;gap:15px;margin-bottom:5px;">
+                    <select id="<?php echo $t->get_field_id( 'widget_xl_cols' ); ?>" name="<?php echo $t->get_field_name( 'widget_xl_cols' ); ?>">
+                        <option value=""><?php echo esc_html__( '-- Extra Large Columns --', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_xl_cols'], 'col-xl-2' ); ?> value="col-xl-2"><?php echo esc_html__( 'col-xl-2', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_xl_cols'], 'col-xl-3' ); ?> value="col-xl-3"><?php echo esc_html__( 'col-xl-3', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_xl_cols'], 'col-xl-4' ); ?> value="col-xl-4"><?php echo esc_html__( 'col-xl-4', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_xl_cols'], 'col-xl-5' ); ?> value="col-xl-5"><?php echo esc_html__( 'col-xl-5', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_xl_cols'], 'col-xl-6' ); ?> value="col-xl-6"><?php echo esc_html__( 'col-xl-6', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_xl_cols'], 'col-xl-7' ); ?> value="col-xl-7"><?php echo esc_html__( 'col-xl-7', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_xl_cols'], 'col-xl-8' ); ?> value="col-xl-8"><?php echo esc_html__( 'col-xl-8', 'newsfit' ); ?></option>
+                    </select>
 
-                <select id="<?php echo $t->get_field_id( 'widget_lg_cols' ); ?>" name="<?php echo $t->get_field_name( 'widget_lg_cols' ); ?>">
-                    <option value=""><?php echo esc_html__( '-- Large Columns --', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_lg_cols'], 'col-lg-2' ); ?> value="col-lg-2"><?php echo esc_html__( 'col-lg-2', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_lg_cols'], 'col-lg-3' ); ?> value="col-lg-3"><?php echo esc_html__( 'col-lg-3', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_lg_cols'], 'col-lg-4' ); ?> value="col-lg-4"><?php echo esc_html__( 'col-lg-4', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_lg_cols'], 'col-lg-5' ); ?> value="col-lg-5"><?php echo esc_html__( 'col-lg-5', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_lg_cols'], 'col-lg-6' ); ?> value="col-lg-6"><?php echo esc_html__( 'col-lg-6', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_lg_cols'], 'col-lg-7' ); ?> value="col-lg-7"><?php echo esc_html__( 'col-lg-7', 'newsfit' ); ?></option>
-                    <option <?php selected( $instance['widget_lg_cols'], 'col-lg-8' ); ?> value="col-lg-8"><?php echo esc_html__( 'col-lg-8', 'newsfit' ); ?></option>
-                </select>
+                    <select id="<?php echo $t->get_field_id( 'widget_lg_cols' ); ?>" name="<?php echo $t->get_field_name( 'widget_lg_cols' ); ?>">
+                        <option value=""><?php echo esc_html__( '-- Large Columns --', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_lg_cols'], 'col-lg-2' ); ?> value="col-lg-2"><?php echo esc_html__( 'col-lg-2', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_lg_cols'], 'col-lg-3' ); ?> value="col-lg-3"><?php echo esc_html__( 'col-lg-3', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_lg_cols'], 'col-lg-4' ); ?> value="col-lg-4"><?php echo esc_html__( 'col-lg-4', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_lg_cols'], 'col-lg-5' ); ?> value="col-lg-5"><?php echo esc_html__( 'col-lg-5', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_lg_cols'], 'col-lg-6' ); ?> value="col-lg-6"><?php echo esc_html__( 'col-lg-6', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_lg_cols'], 'col-lg-7' ); ?> value="col-lg-7"><?php echo esc_html__( 'col-lg-7', 'newsfit' ); ?></option>
+                        <option <?php selected( $instance['widget_lg_cols'], 'col-lg-8' ); ?> value="col-lg-8"><?php echo esc_html__( 'col-lg-8', 'newsfit' ); ?></option>
+                    </select>
+                </div>
+                <small><?php echo esc_html__( 'Column option works only for the footer widgets.', 'newsfit' ); ?></small>
             </div>
-            <small><?php echo esc_html__( 'Total 12 columns grid. The specified column options are optimized for larger displays. When applying this class to a singular widget, it is imperative to extend its usage consistently across other widgets.', 'newsfit' ); ?></small>
-            </p>
+            <style>
+                .rt-widget-custom-cols {
+                    display: none;
+                }
+
+                [data-widget-area-id="rt-footer-sidebar"] .rt-widget-custom-cols {
+                    display: block !important;
+                }
+            </style>
 			<?php
 			$retrun = null;
 
